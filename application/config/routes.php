68058c4f17d2e6b20c45ec1587c165ceee886dff
Celia <Celia@Cee>
2016-01-30 21:08:25 -0800

    Link5

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,7 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 $route['sleep'] = "first/zzz";//link 3 
+$route['show/[0-9]'] = "first/gimme/3"; //link 5
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
